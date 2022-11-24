93c3b35918635c6a493625359640cec0b62e8e7e
Kut Zhang <kut@sanlea.cn>
2022-11-24 12:03:32 +0800

    remove debug codes

--- src/endpoints/helloworld/endpoints.go
+++ src/endpoints/helloworld/endpoints.go
@@ -1,7 +1,6 @@
 package helloworld
 
 import (
-	"log"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -9,7 +8,6 @@ import (
 )
 
 func init() {
-	log.Println("Endpoints loaded: helloworld")
 	server.Server.GET("/helloworld", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Show me the money")
 	})
