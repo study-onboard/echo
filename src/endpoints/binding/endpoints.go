66279a1f2da5f80b6b3777c112c58202d58059ea
Kut Zhang <kut@sanlea.cn>
2022-11-24 16:22:40 +0800

    1. header 2. all binds

--- src/endpoints/binding/endpoints.go
+++ src/endpoints/binding/endpoints.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/study-onboard/echo/src/server"
+	"github.com/study-onboard/echo/src/vendors/web"
 )
 
 func init() {
@@ -57,5 +58,56 @@ func init() {
 		}
 
 		return c.JSONPretty(http.StatusOK, form, "   ")
+	})
+
+	// Header
+	group.DELETE("/header/:id", func(c echo.Context) error {
+		type operateForm struct {
+			Id   string `param:"id"`
+			From string `           header:"X-FORWARD-IP"`
+		}
+
+		var form operateForm
+		binder := &echo.DefaultBinder{}
+
+		// Bind mehtod not support for bind header
+		if err := binder.BindPathParams(c, &form); err != nil {
+			return err
+		}
+
+		// must special
+		if err := binder.BindHeaders(c, &form); err != nil {
+			return err
+		}
+
+		return c.JSONPretty(http.StatusOK, form, "    ")
+	})
+
+	group.POST("/all/:id", func(c echo.Context) error {
+		type allForm struct {
+			Id   string `param:"id"`
+			Ip   string `           header:"X-FORWARD-IP"`
+			From string `                                 query:"from"`
+			Name string `                                              json:"name"`
+		}
+
+		var form allForm
+		if err := web.BindForm(c, &form); err != nil {
+			return err
+		}
+
+		type information struct {
+			Id   string `json:"id"`
+			Ip   string `json:"ip"`
+			From string `json:"from"`
+			Name string `json:"name"`
+		}
+
+		return c.JSONPretty(http.StatusOK, &information{
+			Id:   form.Id,
+			Ip:   form.Ip,
+			From: form.From,
+			Name: form.Name,
+		}, "    ")
 	})
 }
---
+++ src/vendors/web/extensions.go
@@ -0,0 +1,33 @@
+package web
+
+import "github.com/labstack/echo/v4"
+
+// form binder
+var formBinder = &echo.DefaultBinder{}
+
+// Bind form
+//
+// bind request parameters to form:
+// 1. header
+// 2. query
+// 3. body
+// 4. Path
+func BindForm(c echo.Context, form any) error {
+	if err := formBinder.BindHeaders(c, form); err != nil {
+		return err
+	}
+
+	if err := formBinder.BindQueryParams(c, form); err != nil {
+		return err
+	}
+
+	if err := formBinder.BindBody(c, form); err != nil {
+		return err
+	}
+
+	if err := formBinder.BindPathParams(c, form); err != nil {
+		return err
+	}
+
+	return nil
+}
