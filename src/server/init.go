38bcbb13ce424c0cd61cf87115048b52c446eb22
Kut Zhang <kutzhang@gmail.com>
2022-11-25 22:27:33 +0800

    add comment for study

--- src/server/init.go
+++ src/server/init.go
@@ -7,14 +7,24 @@ import (
 	"github.com/study-onboard/echo/src/vendors/web"
 )
 
+// server instance
 var Server *echo.Echo = echo.New()
 
 func init() {
+	// debug mode
 	Server.Debug = true
+
+	// custom HTTP error handler
 	Server.HTTPErrorHandler = web.ErrorHandler
+
+	// custom form validator
 	Server.Validator = &web.RequestValidator{
 		Provider: validator.New(),
 	}
+
+	// use logger middleware
 	Server.Use(middleware.Logger())
+
+	// use recover moddleware
 	Server.Use(middleware.Recover())
 }
